334d4292eb764aef6a02877deaab0f42f8ad7be3
Rehan Dominic <56755435+rehandominic@users.noreply.github.com>
2022-12-06 13:32:05 +0530

    Create order-by-demo.sql

---
+++ tsql13-order-by-keyword/order-by-demo.sql
@@ -0,0 +1,38 @@
+-- Viewing the sample table data
+
+SELECT * FROM SAMPLE_DATA;
+
+-- ORDER BY KEYWORD (Order by Age Ascending)
+
+SELECT * 
+FROM SAMPLE_DATA
+ORDER BY age;
+-- NOTE : Repeat data is ordered by existing order
+
+-- ORDER BY KEYWORD DESC (Order by Age Descending)
+
+SELECT * 
+FROM SAMPLE_DATA
+ORDER BY age DESC;
+
+-- COMPOUND ORDER BY STATEMENTS
+
+SELECT *
+FROM SAMPLE_DATA
+ORDER BY age, first_name;
+
+SELECT id, age, first_name
+FROM SAMPLE_DATA
+ORDER BY age, first_name;
+
+SELECT id, age, first_name
+FROM SAMPLE_DATA
+ORDER BY age;
+
+SELECT id, age, first_name
+FROM SAMPLE_DATA
+ORDER BY age, first_name DESC;
+
+SELECT id, age, first_name
+FROM SAMPLE_DATA
+ORDER BY age DESC, first_name DESC;
