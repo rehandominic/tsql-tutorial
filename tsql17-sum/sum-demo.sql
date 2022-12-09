ec39d81d23862df85448de6e128187435ba92627
Rehan Dominic <56755435+rehandominic@users.noreply.github.com>
2022-12-09 21:47:59 +0530

    Create sum-demo.sql

---
+++ tsql17-sum/sum-demo.sql
@@ -0,0 +1,20 @@
+-- View Sample Data
+
+SELECT * 
+FROM SAMPLE_DATA;
+
+-- SUM() Function Basic Usage
+
+SELECT SUM(age)
+FROM SAMPLE_DATA;
+
+-- SUM() Function Usage with ALIAS
+
+SELECT AVG(age) AS 'TOTAL AGE'
+FROM SAMPLE_DATA;
+
+-- SUM() Function with Filtering using WHERE Clause
+
+SELECT AVG(age) AS 'TOTAL CHINESE AGE'
+FROM SAMPLE_DATA
+WHERE country = 'China';
