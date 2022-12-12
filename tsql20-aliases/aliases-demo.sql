f0291f161e8be1ad396841634b8b03b85bfb09a2
Rehan Dominic <56755435+rehandominic@users.noreply.github.com>
2022-12-13 00:49:39 +0530

    Create aliases-demo.sql

---
+++ tsql20-aliases/aliases-demo.sql
@@ -0,0 +1,18 @@
+-- Basic Alias Usage in SQL Server
+
+SELECT country AS Nationality
+FROM SAMPLE_DATA;
+
+-- If you want the Alias to contain spaces you must enclose the alias within [] or ''
+
+SELECT first_name AS [First Name]
+FROM SAMPLE_DATA;
+
+SELECT first_name AS [First Name], last_name AS 'Last Name'
+FROM SAMPLE_DATA;
+
+-- Combining Columns and Giving the resulting column an alias
+
+SELECT first_name + ' ' + last_name AS [Full Name]
+FROM SAMPLE_DATA;
+
