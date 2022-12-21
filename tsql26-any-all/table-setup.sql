d7ae1836a8fb5cd4b112d219e393133f0fae98cd
Rehan Dominic <56755435+rehandominic@users.noreply.github.com>
2022-12-21 19:03:31 +0530

    Create table-setup.sql

---
+++ tsql26-any-all/table-setup.sql
@@ -0,0 +1,33 @@
+-- CREATING MANAGERS TABLE
+
+create table managers (
+	name VARCHAR(50),
+	age INT
+);
+
+-- INSERTING DATA INTO MANAGERS TABLE
+
+insert into managers (name, age) values ('Davon', 17);
+insert into managers (name, age) values ('Odella', 66);
+insert into managers (name, age) values ('Aristotle', 30);
+insert into managers (name, age) values ('Xylia', 73);
+insert into managers (name, age) values ('Johnnie', 37);
+insert into managers (name, age) values ('George', 49);
+
+------------------------------------------------------------------------
+
+-- CREATING EMPLOYEES TABLE
+
+create table employees (
+	name VARCHAR(50),
+	age INT
+);
+
+-- INSERTING DATA INTO EMPLOYEES TABLE
+
+insert into employees (name, age) values ('Joella', 51);
+insert into employees (name, age) values ('Nicolais', 57);
+insert into employees (name, age) values ('Nedda', 83);
+insert into employees (name, age) values ('Beckie', 11);
+insert into employees (name, age) values ('Welch', 33);
+insert into employees (name, age) values ('Lola', 66);
