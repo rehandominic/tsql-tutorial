4c0ac992658e9fbfe68dd722bdb9eed70b7e7928
Rehan Dominic <56755435+rehandominic@users.noreply.github.com>
2022-12-24 10:28:52 +0530

    Create table-setup.sql

---
+++ tsql28-insert-into-select/table-setup.sql
@@ -0,0 +1,86 @@
+-- Create Sample Table MEMBERS 
+
+create table members (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	gender VARCHAR(50)
+);
+
+-- Insert Data into Sample Table MEMBERS
+
+insert into members (id, first_name, last_name, email, gender) values (1, 'Berry', 'McNay', 'bmcnay0@census.gov', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (2, 'Faina', 'Greave', 'fgreave1@hugedomains.com', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (3, 'Cordy', 'Moncrefe', 'cmoncrefe2@tuttocitta.it', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (4, 'Gaston', 'Nowlan', 'gnowlan3@cyberchimps.com', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (5, 'Cordy', 'Lipp', 'clipp4@eventbrite.com', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (6, 'Hillier', 'Struttman', 'hstruttman5@stumbleupon.com', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (7, 'Jard', 'Stetson', 'jstetson6@etsy.com', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (8, 'Reeba', 'Stillman', 'rstillman7@spiegel.de', 'Non-binary');
+insert into members (id, first_name, last_name, email, gender) values (9, 'Jed', 'Lowden', 'jlowden8@whitehouse.gov', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (10, 'Tamiko', 'Ferbrache', 'tferbrache9@gov.uk', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (11, 'Alyssa', 'Simonel', 'asimonela@diigo.com', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (12, 'Jeane', 'Severy', 'jseveryb@nymag.com', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (13, 'Cami', 'Haycock', 'chaycockc@archive.org', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (14, 'Wally', 'Broadhurst', 'wbroadhurstd@moonfruit.com', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (15, 'Norina', 'Clemetts', 'nclemettse@amazonaws.com', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (16, 'Bella', 'Mintram', 'bmintramf@theatlantic.com', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (17, 'Clayton', 'Parade', 'cparadeg@aol.com', 'Genderqueer');
+insert into members (id, first_name, last_name, email, gender) values (18, 'Barr', 'Desporte', 'bdesporteh@ehow.com', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (19, 'Weider', 'Claughton', 'wclaughtoni@nationalgeographic.com', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (20, 'Joana', 'Peasgood', 'jpeasgoodj@php.net', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (21, 'Anitra', 'Hanse', 'ahansek@homestead.com', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (22, 'Ninon', 'Patis', 'npatisl@sogou.com', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (23, 'Violette', 'Wiggam', 'vwiggamm@state.gov', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (24, 'Elladine', 'Fliege', 'efliegen@va.gov', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (25, 'Colver', 'Sherrett', 'csherretto@npr.org', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (26, 'Ferguson', 'Houtbie', 'fhoutbiep@vinaora.com', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (27, 'Glenden', 'Learoid', 'glearoidq@guardian.co.uk', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (28, 'Janot', 'Woollard', 'jwoollardr@homestead.com', 'Bigender');
+insert into members (id, first_name, last_name, email, gender) values (29, 'Elenore', 'Medley', 'emedleys@kickstarter.com', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (30, 'Valle', 'Hayto', 'vhaytot@slideshare.net', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (31, 'Dorian', 'Eykelhof', 'deykelhofu@youtube.com', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (32, 'Yalonda', 'Walford', 'ywalfordv@ehow.com', 'Genderfluid');
+insert into members (id, first_name, last_name, email, gender) values (33, 'Brodie', 'Ingray', 'bingrayw@geocities.com', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (34, 'Nathalia', 'Jain', 'njainx@yellowpages.com', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (35, 'Sheena', 'Carrington', 'scarringtony@uol.com.br', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (36, 'Raynard', 'Ransome', 'rransomez@walmart.com', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (37, 'Fredek', 'Cloake', 'fcloake10@yale.edu', 'Bigender');
+insert into members (id, first_name, last_name, email, gender) values (38, 'Haskel', 'Lebbon', 'hlebbon11@spiegel.de', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (39, 'Aguistin', 'Leatherbarrow', 'aleatherbarrow12@alexa.com', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (40, 'Kimball', 'Beckett', 'kbeckett13@nymag.com', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (41, 'Raviv', 'Greatham', 'rgreatham14@php.net', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (42, 'Dino', 'Brayshay', 'dbrayshay15@skype.com', 'Genderfluid');
+insert into members (id, first_name, last_name, email, gender) values (43, 'Taber', 'Witherup', 'twitherup16@seattletimes.com', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (44, 'Thurstan', 'Wythill', 'twythill17@e-recht24.de', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (45, 'Brockie', 'Nutkins', 'bnutkins18@prlog.org', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (46, 'Camella', 'Realff', 'crealff19@marriott.com', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (47, 'Janela', 'Morling', 'jmorling1a@reverbnation.com', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (48, 'April', 'Amiable', 'aamiable1b@google.com.hk', 'Female');
+insert into members (id, first_name, last_name, email, gender) values (49, 'Lovell', 'Behnke', 'lbehnke1c@unc.edu', 'Male');
+insert into members (id, first_name, last_name, email, gender) values (50, 'Calypso', 'Goodere', 'cgoodere1d@ocn.ne.jp', 'Genderqueer');
+
+-- Create Sample Table MEMBERS2 with same structure as MEMBERS
+
+create table members2 (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	gender VARCHAR(50)
+);
+
+-- Create Sample Table MEMBERS_DETAILS with only specific columns from members
+
+create table member_details (
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50)
+);
+
+-- Insert a few rows of data into MEMBERS_DETAILS
+
+insert into member_details (first_name, last_name, email) values ('Gusella', 'Dresche', 'gdresche0@bing.com');
+insert into member_details (first_name, last_name, email) values ('Gerard', 'Bilton', 'gbilton1@independent.co.uk');
+insert into member_details (first_name, last_name, email) values ('Fawne', 'Cuss', 'fcuss2@meetup.com');
